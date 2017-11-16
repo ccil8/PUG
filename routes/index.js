5b1203e4788a5516cde05ada8e7fd427fdb953d4
ccil8 <cecile.ribault@laposte.net>
2017-11-16 15:33:36 +0100

    Quete Can i put some delete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const multer  = require('multer');
+const upload = multer({ dest: 'tmp/' });
+const fs = require('fs'); 
 
-/* GET home page. */
-router.get('/coucou-pug', function(req, res, next) {
+/*GET home page.*/
+/*router.get('/coucou-pug', function(req, res, next) {
   res.render('coucou', { title: 'Coucou le monde !', sayHello: 'Hello buddy' });
-});
+});*/
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,29 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('Hey ! It\'s a GET');
-});
+});*/
 
 /* GET user with ID. */
-router.get('/:id(\\d+)', function(req, res, next) {
+/*router.get('/:id(\\d+)', function(req, res, next) {
   res.send('Hey ! It\'s a GET with ID ' + req.params.id);
-});
+});*/
 
 /* PUT user . */
-router.put('/:name(\[a-z]*)', function (req, res, next) {
+/*router.put('/:name(\[a-z]*)', function (req, res, next) {
   res.send('Hey my name is ' + req.params.name );
-});
+});*/
 
 /* POST user creation. */
-router.post('/', function(req, res, next) {
+/*router.post('/', function(req, res, next) {
   res.send('Hey ! It\'s a POST');
-});
+});*/
 
 /* DELETE user creation. */
-router.delete('/:id(\\d+)', function (req, res, next) {
+/*router.delete('/:id(\\d+)', function (req, res, next) {
   res.send('Hey it\'s a delete ID ' + req.params.id);
+});*/
+
+
+router.get('/:(\[a-z]*)', function(req, res, next) {
+  res.render('update-user');
 });
 
+router.put('/:user', function(req, res){
+  res.send('The new name is' + req.body.prenom)
+});
+
+router.get('/:(\\d+)',function(req,res,next){
+  res.render('delete-user')
+});
+
+router.delete('/:id',function(req,res){
+  console.log(req.body);
+  res.send('No more user with id' + req.body.num)
+});
 
 module.exports = router;
